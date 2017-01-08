f6ae3a256956b76521d20e3d23f406457b22e468
liangyf <liangyanfeng0202@163.com>
2017-01-08 18:50:09 +0800

    功能已基本全部实现 就是样子有点丑

--- js/jquery.select.js
+++ js/jquery.select.js
@@ -10,26 +10,56 @@
 			alert(val);
 		},
 		data: {
-			zg: '中国',
-			mg: '美国',
-			yg: '英国',
-			hg: '韩国',
-			rb: '日本'
+
 		}
 	};
+	var only = 'lyf';
 	$.fn.multipleSelect = function(options) {
-		//		this.attr('disabled',true);
-		
-		var t ;
 		this.attr('readonly', true);
-		var $that = this;
-		var opts = $.extend({}, defaults, options);
-		var top = this.offset().top;
-		var left = this.offset().left;
-		var width = this.width();
-		var height = this.height();
-		var only = 'lyf';
+		mutipleSelectInit(this, options);
+	};
+
+	$.fn.multipleSelectSetData = function(dataOption) {
+		$that = this;
+		$that.attr('isFullChoose',false);
+		$that.val('');
 		var inputSelectId = this.attr('id');
+		var ulId = inputSelectId + only + 'ul';
+		$('#' + ulId).empty();
+		$.each(dataOption, function(key, value) {
+			$('#' + ulId).append('<li style="list-style-type:none;"><label><input type="checkbox" key=' + key + ' value= ' + value + ' />' + value + '</label></li>');
+		});
+		var $selectInput = $('#' + ulId + ' input');
+		var selectAllId = inputSelectId + only + 'selectAll';
+		var $selectAllInput = $('#' + selectAllId);
+		$selectAllInput.attr('checked',false);
+		var $selectInputChecked;
+		$selectInput.click(function() {
+			$selectInputChecked = $('#' + ulId + " :checked");
+			setInputVal($selectInput, $selectInputChecked, $that, $selectAllInput);
+		});
+
+		$selectAllInput.click(function() {
+			$selectInputChecked = $('#' + ulId + " :checked");
+			clickSelectAll($(this), $selectInput, $that, $selectInputChecked);
+		});
+		var selectSearchInputId = inputSelectId + only + 'Search';
+		var $selectSearch = $('#' + selectSearchInputId);
+		$selectSearch.val('');
+		$selectSearch.keyup(function(event) {
+			keyupSelectSearch($selectInput,$(this));
+		});
+	}
+
+	function mutipleSelectInit($sel, options) {
+		var t;
+		var $that = $sel;
+		var opts = $.extend({}, defaults, options);
+		var top = $that.offset().top;
+		var left = $that.offset().left;
+		var width = $that.width();
+		var height = $that.height();
+		var inputSelectId = $that.attr('id');
 		var divId = inputSelectId + only + 'Div';
 		var ulId = inputSelectId + only + 'ul';
 		var $ul;
@@ -45,73 +75,56 @@
 		$('body').append('<div style="overflow: auto;" id="' + selectValueShowId + '"></div>');
 		var $selectValue = $('#' + selectValueShowId);
 		$selectValue.css('position', 'absolute');
-		$selectValue.css('top', top + height + 4 + 'px');
+		$selectValue.css('top', top + height + 6 + 'px');
 		$selectValue.css('left', left + 'px');
 		$selectValue.css('width', opts.width);
 		$selectValue.css('height', opts.height);
-		$selectValue.css('background', 'green');
+		$selectValue.css('background', 'white');
+		$selectValue.css('border','1px solid #5B5B5B');
 		$selectValue.hide();
 		$selectValue.append('<ul style="margin: 0;padding: 5px 8px;" id=' + selectValueShowUlId + '></ul>');
 		var $selectValueShowUl = $('#' + selectValueShowUlId);
 
-		$('body').append('<div class="mt" id="' + divId + '"></div>');
+		$('body').append('<div class="selectDiv" id="' + divId + '"></div>');
 		var $selectDiv = $('#' + divId);
 		$selectDiv.css('position', 'absolute');
-		$selectDiv.css('top', top + height + 4 + 'px');
+		$selectDiv.css('top', top + height + 6 + 'px');
 		$selectDiv.css('left', left + 'px');
 		$selectDiv.css('width', opts.width);
 		$selectDiv.css('height', opts.height);
-		$selectDiv.css('background', 'green');
+		$selectDiv.css('background', 'white');
+		$selectDiv.css('border','1px solid #5B5B5B');
+		
 		$selectDiv.hide();
-		if(opts.filter) {
-			$selectDiv.append('<input id="' + selectSearchInputId + '" style="width:' + (opts.width - 5) + 'px" type="text" />');
-			$selectSearch = $('#' + selectSearchInputId);
-		}
-		$selectDiv.append('<input style="margin-left: 12px;margin-top:5px;" id="' + selectAllId + '" type="checkbox" />全选');
-		$selectAllInput = $('#' + selectAllId);
-		if(opts.selectAll) {
-			$selectAllInput.show();
-		} else {
-			$selectAllInput.hide();
-		}
-		$selectDiv.append('<ul style="margin: 0;padding: 5px 8px;" id=' + ulId + '></ul>');
-		$ul = $('#' + ulId);
-		$.each(opts.data, function(key, value) {
-			$ul.append('<li style="list-style-type:none;"><label><input type="checkbox" key=' + key + ' value= ' + value + ' />' + value + '</label></li>');
-		});
-		$selectInput = $('#' + ulId + ' input');
-		$selectInputChecked = $('#' + ulId + " :checked");
 
-		this.click(function(ev) {
+		$that.click(function(ev) {
 			if($selectDiv.is(":hidden")) {
-				$('.mt').hide();
-				$selectDiv.show(); //如果元素为隐藏,则将它显现
+				$('.selectDiv').hide();
+				$selectDiv.show();
 			} else {
-				$selectDiv.hide(); //如果元素为显现,则将其隐藏
+				$selectDiv.hide();
 			}
 			return false;
 		});
 		$selectDiv.click(function(ev) {
 			var oEvent = ev || event;
 			oEvent.stopPropagation();
-		})
-		document.onclick = function() {
-			//			alert($selectDiv.is(":hidden"));
-			$('.mt').hide();
-
-		}
-		$selectInput.click(function() {
-			setInputVal();
 		});
+		document.onclick = function() {
+			$('.selectDiv').hide();
+		}
 
 		$that.mouseenter(function() {
-			t = setTimeout(function(){
-				var v = $that.val().split(',');
-				$.each(v, function(key, value) {
-					$selectValueShowUl.append('<li style="list-style-type:none;">' + value + '</li>');
-				});
-				$selectValue.show();
-			},1000);
+			if(!$selectAllInput.attr('checked') && $that.val() != '') {
+				t = setTimeout(function() {
+					var v = $that.val().split(',');
+					$.each(v, function(key, value) {
+						$selectValueShowUl.append('<li style="list-style-type:none;">' + value + '</li>');
+					});
+					$selectValue.show();
+				}, 1000);
+			}
+
 		});
 
 		$that.mouseleave(function() {
@@ -120,69 +133,104 @@
 			$selectValue.hide();
 		});
 
+		$selectDiv.append('<input id="' + selectSearchInputId + '" class="searchInput" type="text" />');
+		$selectSearch = $('#' + selectSearchInputId);
+		$selectSearch.css('width', opts.width - 5);
+		if(opts.filter) {
+			$selectSearch.keyup(function(event) {
+				keyupSelectSearch($selectInput,$(this));
+			});
+			$selectSearch.show();
+		} else {
+			$selectSearch.hide();
+		}
+
+		$selectDiv.append('<input style="margin-left: 12px;margin-top:5px;" id="' + selectAllId + '" type="checkbox" />全选');
+		$selectAllInput = $('#' + selectAllId);
+		$that.attr('isFullChoose',false);
+		if(opts.selectAll) {
+			$selectAllInput.show();
+		} else {
+			$selectAllInput.hide();
+		}
+
 		$selectAllInput.click(function() {
-			if($(this).attr('checked')) {
-				$selectInput.attr('checked', true);
-				$that.val('全选');
-				setKey();
-			} else {
-				$selectInput.attr('checked', false);
-				$that.val('');
-				$that.attr('key', '');
-			}
+			$selectInputChecked = $('#' + ulId + " :checked");
+			clickSelectAll($(this), $selectInput, $that, $selectInputChecked);
 		});
 
-		$selectSearch.keyup(function(event) {
-			var content = $.trim($selectSearch.val());
-			console.log(content);
-			$selectInput.each(function() {
-				if($(this).attr('value').indexOf(content) == -1) {
-					$(this).parent().hide();
-				} else {
-					$(this).parent().show();
-				}
-			});
+		$selectDiv.append('<ul style="margin: 0;padding: 5px 8px;" id=' + ulId + '></ul>');
+		$ul = $('#' + ulId);
+
+		$.each(opts.data, function(key, value) {
+			$ul.append('<li style="list-style-type:none;"><label><input type="checkbox" key=' + key + ' value= ' + value + ' />' + value + '</label></li>');
 		});
 
-		function setInputVal() {
+		$selectInput = $('#' + ulId + ' input');
+		$selectInput.click(function() {
 			$selectInputChecked = $('#' + ulId + " :checked");
-			if($selectInput.length == $selectInputChecked.length) {
+			setInputVal($selectInput, $selectInputChecked, $that, $selectAllInput);
+		});
 
-				setKey();
-				$selectAllInput.attr('checked', true);
-				$that.val('全选');
-			} else if($selectInputChecked.length == 0) {
-				$that.val('');
-				$selectAllInput.attr('checked', false);
-			} else {
-				setKey();
-				var v = '';
-				$selectInputChecked.each(function() {
-					v += $(this).attr('value') + ',';
-				});
-				v = v.substr(0, v.length - 1);
-				$that.val(v);
-				$selectAllInput.attr('checked', false);
-			}
-		}
+	}
 
-		function setKey() {
-			$selectInputChecked = $('#' + ulId + " :checked");
-			var k = '';
+	function setInputVal($selectInput, $selectInputChecked, $that, $selectAllInput) {
+
+		if($selectInput.length == $selectInputChecked.length) {
+			setKey($selectInputChecked, $that);
+			$selectAllInput.attr('checked', true);
+			$that.attr('isFullChoose',true);
+			$that.val('全选');
+		} else if($selectInputChecked.length == 0) {
+			$that.val('');
+			$selectAllInput.attr('checked', false);
+			$that.attr('isFullChoose',false);
+		} else {
+			setKey($selectInputChecked, $that);
+			var v = '';
 			$selectInputChecked.each(function() {
-				k += $(this).attr('key') + ',';
+				v += $(this).attr('value') + ',';
 			});
-			k = k.substr(0, k.length - 1);
-			$that.attr('key', k);
+			v = v.substr(0, v.length - 1);
+			$that.val(v);
+			$selectAllInput.attr('checked', false);
+			$that.attr('isFullChoose',false);
 		}
-	};
+	}
 
-	$.fn.multipleSelectSetData = function(dataOption) {
-		var ulId = this.attr('id') + 'ul';
-		$('#' + ulId).empty();
-		$.each(dataOption, function(key, value) {
-			$('#' + ulId).append('<li style="list-style-type:none;"><label><input type="checkbox" key=' + key + ' value= ' + value + ' />' + value + '</label></li>');
+
+	function clickSelectAll($selectAllInput, $selectInput, $that) {
+		if($selectAllInput.attr('checked')) {
+			$selectInput.attr('checked', true);
+			$that.val('全选');
+			$that.attr('isFullChoose',true);
+			setKey($selectInput, $that);
+		} else {
+			$selectInput.attr('checked', false);
+			$that.val('');
+			$that.attr('key', '');
+			$that.attr('isFullChoose',false);
+		}
+	}
+
+	function keyupSelectSearch($selectInput,$selectSearch) {
+		var content = $.trim($selectSearch.val());
+		$selectInput.each(function() {
+			if($(this).attr('value').indexOf(content) == -1) {
+				$(this).parent().hide();
+			} else {
+				$(this).parent().show();
+			}
 		});
+	}
+	
+	function setKey($selectInputChecked, $that) {
+		var k = '';
+		$selectInputChecked.each(function() {
+			k += $(this).attr('key') + ',';
+		});
+		k = k.substr(0, k.length - 1);
+		$that.attr('key', k);
 	}
 
 })(jQuery);
